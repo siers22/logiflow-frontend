e18f2452e975b17a826fc6254a1fcbc9af0e5566
siers22 <siers6292@gmail.com>
2026-03-18 10:55:25 +0500

    поменял задний фон, теепрь меньше нагружает систему

--- src/components/BackgroundAnimation.tsx
+++ src/components/BackgroundAnimation.tsx
@@ -1,5 +1,12 @@
 import "./BackgroundAnimation.css";
 
 export function BackgroundAnimation() {
-  return <div className="background-animation" aria-hidden="true" />;
+  return (
+    <div className="bg-root" aria-hidden="true">
+      <div className="bg-blob bg-blob-1" />
+      <div className="bg-blob bg-blob-2" />
+      <div className="bg-blob bg-blob-3" />
+      <div className="bg-blob bg-blob-4" />
+    </div>
+  );
 }
